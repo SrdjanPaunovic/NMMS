28e7e818c0f255bd7270a9694acd638b82818af3
SrdjanPaunovic <srdjan.paunovic94@gmail.com>
2016-12-26 17:36:20 +0100

    Create initial Clien project with Login view,add theme and necessary resources

---
+++ Hiring Company/Client/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Client.ViewModel
+{
+	public class MainWindowViewModel
+	{
+		#region Fields
+		private ICommand loginCommand;
+		#endregion Fields
+
+		#region Properties
+		public ICommand LoginCommand
+		{
+			get
+			{
+				return loginCommand ?? (loginCommand = new RelayCommand(param => this.LoginClick(param)));
+			}
+		}
+		#endregion Properties
+
+		#region Methods
+		private void LoginClick(object param)
+		{
+			object[] parameters = param as object[];
+
+			if(parameters == null)
+			{
+				throw new Exception("[LoginCommnad] Command parameters has NULL value");
+			}
+			//TODO
+		}
+		#endregion Methods
+
+	}
+}
