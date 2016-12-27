cc03527bbd58993b6058c8dcc9e0dc5fa4ac1e04
Nemanja <zerajicnemanja1994@gmail.com>
2016-12-27 12:28:29 +0100

    Implemented host on service

---
+++ Hiring Company/Service/HiringCompanyServicecs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceContract;
+
+namespace HiringCompanyService
+{
+	public class HiringCompanyService : IHiringContract
+	{
+
+
+		public bool LogIn(string username, string password)
+		{
+			throw new NotImplementedException();
+
+		}
+
+		public bool LogOut(string username, string password)
+		{
+			throw new NotImplementedException();
+
+		}
+	}
+}
--- Hiring Company/Service/Program.cs
+++ Hiring Company/Service/Program.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceContract;
 
-namespace Service
+namespace HiringCompanyService
 {
 	class Program
 	{
+
+		private static ServiceHost host;
+
 		static void Main(string[] args)
 		{
+			Start();
+			Console.ReadKey(true);
+			Stop();
+
+		}
+
+		private static void Start()
+		{
+			host = new ServiceHost(typeof(HiringCompanyService));
+			host.AddServiceEndpoint(typeof(IHiringContract),
+				new NetTcpBinding(),
+				new Uri("net.tcp://localhost:4000/IHiringContract"));
+			host.Open();
+			Console.WriteLine("Server is ready ! ");
+
+		}
+
+		private static void Stop()
+		{
+
+			host.Close();
 		}
 	}
 }
