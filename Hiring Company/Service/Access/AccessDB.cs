91bbd3b8783ee5dd723af34083a119747b189b5c
Nemanja <zerajicnemanja1994@gmail.com>
2016-12-28 11:57:46 +0100

    Initialization of database.

---
+++ Hiring Company/Service/Access/AccessDB.cs
@@ -0,0 +1,16 @@
+﻿using Service.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Access
+{
+	public class AccessDB:DbContext
+	{
+		public AccessDB() : base("HiringDB") { }
+		public DbSet<User> Users { get; set; }
+	}
+}
---
+++ Hiring Company/Service/Access/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Access
+{
+	class Configuration: DbMigrationsConfiguration<AccessDB>
+	{
+		public Configuration()
+		{
+			AutomaticMigrationsEnabled = true;
+			AutomaticMigrationDataLossAllowed = true;
+			ContextKey = "HiringDB";
+		}
+	}
+}
