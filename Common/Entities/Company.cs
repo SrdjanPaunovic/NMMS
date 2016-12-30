04c2a0a05fd2c2b69ce1f0440e9404890b09a518
Nemanja <zerajicnemanja1994@gmail.com>
2016-12-30 10:37:33 +0100

    Added State property in Company class.
    State represents state od collaboration between companies.

--- Common/Entities/Company.cs
+++ Common/Entities/Company.cs
@@ -6,12 +6,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace Common.Entities
 {
     public class Company
     {
         private int id;
         private string name;
+		private Common.Entities.State.CompanyState state;
 
 		[Key]
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -20,13 +23,19 @@ namespace Common.Entities
 			get { return id; }
 			set { id = value; }
 		}
-        
- 
+         
         public string Name
         {
             get { return name; }
             set { name = value; }
         }
+
+		public Common.Entities.State.CompanyState State
+		{
+			get { return state; }
+			set { state = value; }
+		}
+
 		
     }
 }
