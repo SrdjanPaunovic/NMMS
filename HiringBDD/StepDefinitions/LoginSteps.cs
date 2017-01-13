97bd53c3c9f9b01f96dc5bbabfcc743cc34baa1a
mazga <marko.bogdanovic2806@gmail.com>
2017-01-13 12:27:06 +0100

    proba

---
+++ HiringBDD/StepDefinitions/LoginSteps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace HiringBDD.StepDefinitions
+{
+    [Binding]
+    public class LoginSteps
+    {
+        [Given(@"I enter valid ""(.*)"" or ""(.*)""")]
+        public void GivenIEnterValidOr(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I enter invalid ""(.*)"" or ""(.*)""")]
+        public void GivenIEnterInvalidOr(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
