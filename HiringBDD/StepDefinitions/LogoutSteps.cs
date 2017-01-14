a2b83dc09bfbd4cabf868c4b479f43559d2c86de
mazga <marko.bogdanovic2806@gmail.com>
2017-01-14 17:08:39 +0100

    Log out feature

---
+++ HiringBDD/StepDefinitions/LogoutSteps.cs
@@ -0,0 +1,33 @@
+﻿using Client;
+using NUnit.Framework;
+using System;
+using System.ServiceModel;
+using TechTalk.SpecFlow;
+
+namespace HiringBDD.StepDefinitions
+{
+    [Binding]
+    public class LogoutSteps
+    {
+		private HiringClientProxy proxy = new HiringClientProxy(new NetTcpBinding(), "net.tcp://localhost:4000/IHiringContract");
+		private bool result;
+
+		[Given(@"I am logged in")]
+        public void GivenIAmLoggedIn()
+        {
+			proxy.LogIn("admin", "admin");
+		}
+
+		[When(@"I click log out")]
+        public void WhenIClickLogOut()
+        {
+			result = proxy.LogOut("admin");
+        }
+        
+        [Then(@"I should be successfully logged out")]
+        public void ThenIShouldBeSuccessfullyLoggedOut()
+        {
+			Assert.AreEqual(true, result);
+        }
+    }
+}
